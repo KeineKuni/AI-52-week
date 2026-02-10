47e62869fe75c066dc82870de9294ea9add37670
Kuni <maple3625@yahoo.com.tw>
2026-02-10 12:01:03 +0800

    260210

--- week0506_2_numpy_test.py
+++ week0506_2_numpy_test.py
@@ -11,3 +11,9 @@ mask = on_sales > 500
 expensive_items = on_sales[mask]
 
 cheap_goods = on_sales[on_sales < 200]
+
+revenue = np.array([10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 110, 120])
+revenue_season = revenue.reshape(4, -1)
+print(revenue_season)
+print(sum(revenue_season))
+print(np.sum(revenue_season))
